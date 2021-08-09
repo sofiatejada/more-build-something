e414b2bce6a082904a5621d4de76517bd574df6d
Sofia Tejada <sofianais@icloud.com>
2021-08-09 10:29:39 -0700

    test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,36 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+const vista = {
+  title: 'Vista',
+  artist: 'iamamiwhoami',
+  album: 'BLUE'
+};
+const never = {
+  title: 'Never Let You Go',
+  artist: 'Georgia',
+  album: 'Seeking Thrills'
+};
+const reckoner = {
+  title: 'Reckoner',
+  artist: 'Radiohead',
+  album: 'In Rainbows'
+};
+
+
+describe('song routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a song', async () => {
+    const res = await request(app)
+      .post('/api/v1/songs')
+      .send(vista);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...vista
+    });
   });
 });
